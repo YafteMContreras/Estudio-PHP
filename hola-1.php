77fbfe97e904dd59cfc12d6dde5bef75fec1d2c1
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-24 15:55:43 -0600

    Elimina archivos innecesarios

--- hola-1.php
+++
@@ -1,4 +0,0 @@
-<?php
-date_default_timezone_set('America/Mexico_City');
-echo "Hola mundo, me llamo Yafte y hoy es: " . date("d-m-Y H:i:s") . "\n";
-?>
--- hola-1v1.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('America/Mexico_City');
-$nombre = "Yafte";
-$fecha = date("d-m-Y H:i:s");
-
-echo "Hola mundo, me llamo $nombre y hoy es: $fecha \n";
-?>
