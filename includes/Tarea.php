4024372af8e5835ca870ea131751fa20f75b9190
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-30 19:47:01 -0600

    Corrige errores de sintaxis

--- includes/Tarea.php
+++ includes/Tarea.php
@@ -1,13 +1,13 @@
 <?php
-public class Tarea{
+class Tarea{
 	public function __construct(
-		public readonly int $id = 0,
 		public string $titulo,
+		public readonly int $id = 0,
 		public int $prioridad = 1,
 		public bool $completada = false
 	) {}
 
-	public class textoPrioridad() : string {
+	public function textoPrioridad() : string {
 		return match($this->prioridad){
 			1 => "Alta", 2 => "Media", 3 => "Baja",
 		};
